2b471caaa6851330f26075d573a3868ff20fecf1
Samuel Llibre Santos <kuzenshi@gmail.com>
2024-03-30 10:02:06 -0300

    feat(utils): ✨ add utility for combining and merging Tailwind CSS classes
    - Create a common/utils folder and a cn.ts file.
    - Implement the cn utility function using clsx and twMerge to effectively combine and merge Tailwind/Uno CSS classes.

---
+++ src/common/utils/cn.ts
@@ -0,0 +1,18 @@
+import { ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+/**
+ * Combines and merges Tailwind CSS classes using twMerge and clsx utility functions.
+ * twMerge is used to handle conflicts between classes effectively.
+ *
+ * @param {...ClassValue} inputs - An array of class values to be combined and merged.
+ * @returns {string} - The merged and combined class names as a string.
+ */
+export const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs))
+}
+
+/**
+ * Source:
+ * Tailwind merge: https://github.com/dcastil/tailwind-merge/tree/v1.14.0
+ */
