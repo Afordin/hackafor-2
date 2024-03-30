f29ecf39598bfd851593083008f162be60ba2158
LauraNicole449 <lauranicolegarcia449@gmail.com>
2024-03-30 11:54:24 +0100

    feat: cursor pointer nav and change color button nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,11 +6,11 @@ export const Nav = () => {
     <nav className="mx-auto flex px-[24px] py-[12px] text-[16px] h-fit border-[0.2px] border-cBorder rounded-full items-center gap-[42px] text-cGray bg-cBackground">
       <img src="./images/logo.png" width={35} height={32}></img>
       <div className="flex items-center gap-3">
-        <p>Inicio</p>
+        <p style={{ cursor: 'pointer' }}>Inicio</p>
         <span className="w-[6px] h-[6px] bg-cGray rounded-full inline-block"></span>
-        <p>Reglamento</p>
+        <p style={{ cursor: 'pointer' }}>Reglamento</p>
         <span className="w-[6px] h-[6px] bg-cGray rounded-full inline-block"></span>
-        <p>Proyectos</p>
+        <p style={{ cursor: 'pointer' }}>Proyectos</p>
         <span className="w-[6px] h-[6px] bg-cGray rounded-full inline-block"></span>
       </div>
       <Button
--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -10,7 +10,7 @@ const VARIANT_PRIMARY_STYLE =
   'bg-gradient-to-rb from-[#FC1C37] to-[#AD40E1] text-[24px] text-cWhite rounded-full hover:from-cWhite hover:to-cWhite hover:text-cBlack font-bold px-[48px] py-[16px]'
 
 const VARIANT_SECONDARY_STYLE =
-  'bg-gradient-to-rb from-[#FC1C37] to-[#AD40E1] text-[16px] text-cWhite rounded-full hover:from-cWhite hover:to-cWhite font-bold px-[14px] py-[10px]'
+  'bg-gradient-to-rb from-[#FC1C37] to-[#AD40E1] text-[16px] text-cWhite rounded-full hover:from-cWhite hover:to-cWhite hover:text-cBlack font-bold px-[14px] py-[10px]'
 
 export const Button = ({ children, onClick, variant }: ButtonProps) => {
   return (
