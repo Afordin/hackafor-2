aa1061a7251a743da2a5146e536d2e9104c3a88f
LauraNicole449 <lauranicolegarcia449@gmail.com>
2024-03-30 13:17:43 +0100

    replace style pointer to tailwind

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,11 +6,11 @@ export const Nav = () => {
     <nav className="mx-auto flex px-[24px] py-[12px] text-[16px] h-fit border-[0.2px] border-cBorder rounded-full items-center gap-[42px] text-cGray bg-cBackground">
       <img src="./images/logo.png" width={35} height={32}></img>
       <div className="flex items-center gap-3">
-        <p style={{ cursor: 'pointer' }}>Inicio</p>
+        <p className="cursor-pointer">Inicio</p>
+        <span className="w-[6px] h-[6px] bg-cGray rounded-full inline-block cursor-pointer"></span>
+        <p className="cursor-pointer">Reglamento</p>
         <span className="w-[6px] h-[6px] bg-cGray rounded-full inline-block"></span>
-        <p style={{ cursor: 'pointer' }}>Reglamento</p>
-        <span className="w-[6px] h-[6px] bg-cGray rounded-full inline-block"></span>
-        <p style={{ cursor: 'pointer' }}>Proyectos</p>
+        <p className="cursor-pointer">Proyectos</p>
         <span className="w-[6px] h-[6px] bg-cGray rounded-full inline-block"></span>
       </div>
       <Button
