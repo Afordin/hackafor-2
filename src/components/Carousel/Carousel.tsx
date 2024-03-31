6c8b65d1ae5ad4201ffe6cd8b4b45815aac30efb
Samuel Llibre Santos <kuzenshi@gmail.com>
2024-03-31 19:33:15 -0300

    feat(components/carousel): ✨ add Carousel component with dynamic content animation
    Implement a new `Carousel` component designed to display an array of
    ReactNode children within a horizontally scrolling animation. This
    component supports dynamic content lengths and incorporates responsive
    design principles to ensure proper display across various device sizes.
    
    The Carousel component introduces:
    - A customizable class name for additional styling flexibility.
    - Dynamic rendering of children elements to facilitate various content
      types.
    - A continuous loop animation achieved by duplicating the children
      array, ensuring seamless transitions at all times.
    
    Accompanying the component, CSS animations (`animate-carousel`) are
    defined for both mobile and desktop views, enabling smooth, linear,
    and infinite scrolling of the content.
    
    This addition enhances the project's UI capabilities, allowing for
    more engaging and interactive displays of content such as logos,
    featured projects, or images.

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,3 +1,58 @@
-export const Carousel = () => {
-  return <div>Carousel</div>;
+import { Children, Fragment, type ReactNode } from 'react';
+import { cn } from '@common';
+
+interface CarouselProps {
+  /**
+   * The CSS class to apply to the component.
+   */
+  className?: string;
+
+  /**
+   * Elements to display inside the Carousel.
+   */
+  children: Array<ReactNode>;
+}
+
+export const Carousel = ({ className, children }: CarouselProps) => {
+  const classes = {
+    container: cn(
+      'h-[25rem] mx-auto max-md:max-w-[35rem] md:w-full',
+      'grid place-items-center',
+      'relative overflow-hidden',
+
+      // Left Fade Out
+      'before:absolute before:top-0 before:left-0 ',
+      'before:h-full before:w-1/6 before:z-10 before:pointer-events-none',
+      'before:bg-gradient-to-r before:content-[""]',
+      'before:from-cBackground before:via-transparent  before:to-transparent',
+
+      // Right Fade out
+      'after:absolute after:top-0 after:-right-1 after:rotate-180',
+      'after:h-full after:w-1/6 after:z-10 after:pointer-events-none',
+      'after:bg-gradient-to-r  after:via-transparent  after:to-transparent after:content-[""]',
+      'after:from-cBackground',
+      className
+    ),
+
+    // NOTE: If you add more projects to the carousel, please update the width
+    innerContainer: cn('w-[150rem] md:w-[200rem] flex justify-between gap-10', 'animate-carousel')
+  };
+  const logos = Children.toArray(children);
+  const renderChildren = logos.map((logo, index) => <Fragment key={index}>{logo}</Fragment>);
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.innerContainer}>
+        {/**
+         * renderChildren is called twice to enable a continuous loop
+         * of logos in the carousel, avoiding blank spaces at the end of
+         * the loop. The duplication ensures an uninterrupted visual flow,
+         * making the transition from the last logo back to the first
+         * seamless.
+         */}
+        {renderChildren}
+        {renderChildren}
+      </div>
+    </div>
+  );
 };
