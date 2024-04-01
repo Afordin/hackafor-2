8afb41200d4bf1bd9db807195ccd14331c62dfdf
Samuel Llibre Santos <kuzenshi@gmail.com>
2024-03-31 23:32:27 -0300

    refactor(components/carousel): ♻️ enhance carousel with dynamic effects
    Enhance Carousel component by integrating useCarouselEffect for dynamic
    interaction effects. Update container height and include CAROUSEL_CONFIG
    for standardized settings. Adjust container class to support new effects.

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,5 +1,5 @@
 import { Children, Fragment, type ReactNode } from 'react';
-import { cn } from '@common';
+import { CAROUSEL_CONFIG, cn, useCarouselEffect } from '@common';
 
 interface CarouselProps {
   /**
@@ -14,34 +14,37 @@ interface CarouselProps {
 }
 
 export const Carousel = ({ className, children }: CarouselProps) => {
+  useCarouselEffect('carousel-container', CAROUSEL_CONFIG);
+
   const classes = {
     container: cn(
-      'h-[25rem] mx-auto max-md:max-w-[35rem] md:w-full',
+      'carousel-container',
+      'h-[30rem] mx-auto max-md:max-w-[35rem] md:w-full',
       'grid place-items-center',
-      'relative overflow-hidden',
+      'relative overflow-hidden ',
 
       // Left Fade Out
       'before:absolute before:top-0 before:left-0 ',
       'before:h-full before:w-1/6 before:z-10 before:pointer-events-none',
       'before:bg-gradient-to-r before:content-[""]',
-      'before:from-cBackground before:via-transparent  before:to-transparent',
+      'before:from-cBackground before:via-transparent before:to-transparent',
 
       // Right Fade out
       'after:absolute after:top-0 after:-right-1 after:rotate-180',
       'after:h-full after:w-1/6 after:z-10 after:pointer-events-none',
-      'after:bg-gradient-to-r  after:via-transparent  after:to-transparent after:content-[""]',
+      'after:bg-gradient-to-r after:via-transparent after:to-transparent after:content-[""]',
       'after:from-cBackground',
       className
     ),
-
     // NOTE: If you add more projects to the carousel, please update the width
     innerContainer: cn('w-[150rem] md:w-[200rem] flex justify-between gap-10', 'animate-carousel')
   };
+
   const logos = Children.toArray(children);
   const renderChildren = logos.map((logo, index) => <Fragment key={index}>{logo}</Fragment>);
 
   return (
-    <div className={classes.container}>
+    <div id="carousel-container" className={classes.container}>
       <div className={classes.innerContainer}>
         {/**
          * renderChildren is called twice to enable a continuous loop
