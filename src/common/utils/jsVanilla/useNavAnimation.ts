32c4f313bbb2dc3fb28121bd39af027dd5247949
Paul Cardenas <paul.cardenas.0002@gmail.com>
2024-04-02 15:47:17 -0500

    Handle scroll to hide and show the top nav

---
+++ src/common/utils/jsVanilla/useNavAnimation.ts
@@ -0,0 +1,66 @@
+import { useEffect, useRef, useState } from 'react';
+
+interface NavAnimationReturn {
+  /**
+   * Whether the user is at the top of the page.
+   */
+  isAtTop: boolean;
+  /**
+   * Whether the navigation bar is hidden because the user is scrolling down.
+   */
+  isHidden: boolean;
+}
+
+/**
+ * This function is used to hide or show the navigation bar
+ * based on the scroll direction.
+ */
+export const useNavAnimation = (): NavAnimationReturn => {
+  const lastScrollTop = useRef<number>(0);
+
+  const [navState, setNavState] = useState<NavAnimationReturn>({
+    isHidden: false,
+    isAtTop: true
+  });
+
+  useEffect(() => {
+    function handleScroll(): void {
+      /**
+       * Only update the state if the values have changed.
+       */
+      const updateNavState = (newValues: NavAnimationReturn) => {
+        if (newValues.isHidden === navState.isHidden && newValues.isAtTop === navState.isAtTop) return;
+        setNavState(newValues);
+      };
+
+      const currentScrollTop = window.scrollY;
+
+      /**
+       * If the user is at the top of the page or
+       * scrolling up, the navigation bar should be visible.
+       * If the user is scrolling down, the navigation bar should be hidden.
+       */
+      if (currentScrollTop === 0 || currentScrollTop < lastScrollTop.current) {
+        updateNavState({
+          isHidden: false,
+          isAtTop: currentScrollTop === 0
+        });
+      } else if (currentScrollTop > lastScrollTop.current) {
+        updateNavState({
+          isHidden: true,
+          isAtTop: false
+        });
+      }
+
+      lastScrollTop.current = currentScrollTop;
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [navState]);
+
+  return navState;
+};
