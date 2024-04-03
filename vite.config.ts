236b3579031448400cafaf998aa925cf73966d6e
Samuel Llibre Santos <kuzenshi@gmail.com>
2024-04-03 20:49:38 -0300

    feat(configs): 🔧 feat: add @layouts alias in vite config and tsconfig
    Added a new alias '@layouts' in both the TypeScript configuration and the Vite
    project configuration. This alias points to './src/layouts' (or './src/layouts/index.ts'
    for TypeScript), streamlining the import process for layout components across
    the project. This change enhances code readability and maintainability by
    simplifying imports and ensuring consistency in how layout components are
    referenced.

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       '@common': path.resolve(__dirname, './src/common/index.ts'),
       '@components': path.resolve(__dirname, './src/components/index.ts'),
       '@data': path.resolve(__dirname, './src/data/index.ts'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
       '@pages': path.resolve(__dirname, './src/pages'),
       '@store': path.resolve(__dirname, './src/store/index.ts')
     }
