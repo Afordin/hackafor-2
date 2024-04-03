482d6a9ac7e7331a74b85561f20405df72f7c206
Jarrison Cano <jarrison.cano@hotmail.com>
2024-04-03 01:08:26 -0500

    feat: add and show user data from supabase login

---
+++ src/components/Navigation/Avatar.tsx
@@ -0,0 +1,58 @@
+import { FC, useState } from 'react';
+import { cn, useAuth } from '@common';
+
+interface Props {
+  /**
+   * Specify an optional className to be added to the component
+   */
+  className?: string;
+
+  /**
+   * Specify the size of the avatar
+   */
+  size?: 'sm' | 'md' | 'lg';
+
+  /**
+   * Specify the avatar
+   */
+  avatar: string;
+}
+
+export const Avatar: FC<Props> = ({ avatar, className, size = 'sm' }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { signOut } = useAuth();
+
+  const classes = {
+    container: ` flex w-fit h-full items-center relative gap-4 ${className} text-white`,
+    sm: 'w-8 h-8',
+    md: 'w-10 h-10',
+    lg: 'w-12 h-12',
+    image: 'rounded-full',
+    imageIcon: 'absolute bottom-[-5px] right-[-5px] text-inherit font-bold text-lg',
+    text: 'flex text-white hover:text-primary-400 items-center gap-3 text-inherit bg-transparent rounded-lg font-semibold text-sm cursor-pointer',
+    modal: 'absolute top-10  right-[-5px] border-1 border-cBorder rounded-lg px-5 py-2 bg-cBackground w-40 cursor-pointer'
+  };
+
+  return (
+    <div className={classes.container}>
+      <span
+        className="relative cursor-pointer"
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        <span className={cn('i-material-symbols-expand-more', classes.imageIcon, isOpen && 'text-yellow')} />
+        <img className={cn(classes[size], classes.image)} src={avatar} alt="avatar" />
+      </span>
+      {/* modal */}
+      {isOpen && (
+        <div className={classes.modal}>
+          <p className={classes.text} onClick={signOut}>
+            <span className="i-material-symbols-exit-to-app-rounded" />
+            Cerrar sesión
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
--- src/pages/_sections/CTA.tsx
+++ src/pages/_sections/CTA.tsx
@@ -1,32 +1,51 @@
-import { ButtonSize, cn, useBreakpoint, VARIANT } from '@common';
+import { ButtonSize, cn, ROUTE, useAuth, useBreakpoint, VARIANT } from '@common';
 import { Button } from '@components';
+import { useUserStore } from '@store';
+import { useNavigate } from 'react-router-dom';
 
 type CTAProps = {
-  children: string;
   className?: string;
 };
 
-export const CTA = ({ children, className }: CTAProps) => {
+export const CTA = ({ className }: CTAProps) => {
+  const navigate = useNavigate();
   const { isMobile } = useBreakpoint();
+  const { signInWithDiscord } = useAuth();
+  const user = useUserStore((state) => state.user);
   const handleButtonSize = isMobile ? ButtonSize.lg : ButtonSize.xl;
+
+  const text = {
+    logged: {
+      message: '¿Quieres participar en la hackaton? ¡Inscríbete!',
+      button: 'Inscribirme ahora'
+    },
+    notLogged: {
+      message: 'Para inscribirte inicia sesión con Discord',
+      button: 'Accede con Discord'
+    }
+  };
 
   const classes = {
     container: cn('relative z-10', className)
   };
   return (
     <article className={classes.container}>
-      <p className="max-sm:text-fluid-sm text-fluid-base">{children}</p>
+      <p className="max-sm:text-fluid-sm text-fluid-base">{user ? text.logged.message : text.notLogged.message}</p>
       <Button
         onClick={() => {
-          console.log('');
+          if (user) {
+            navigate(ROUTE.registration);
+            return;
+          }
+          signInWithDiscord();
         }}
         variant={VARIANT.PRIMARY}
         size={handleButtonSize}
         className="mt-6"
       >
         <div className="flex gap-2 items-center">
-          <span className="i-bi-discord"></span>
-          <span>Accede con Discord</span>
+          {user ? <span className="i-bi-link-45deg text-xl"></span> : <span className="i-bi-discord"></span>}
+          <span>{user ? text.logged.button : text.notLogged.button}</span>
         </div>
       </Button>
     </article>
