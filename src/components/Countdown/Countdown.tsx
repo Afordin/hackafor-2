7b35c63fb58b324d47bd9acf31984bc181ff8c3d
LUISEDOCCOR <luiseduardoocegueda@gmail.com>
2024-04-05 10:13:12 -0600

    feat: add shadow on text

--- src/components/Countdown/Countdown.tsx
+++ src/components/Countdown/Countdown.tsx
@@ -50,7 +50,7 @@ export const Countdown = () => {
   return (
     <>
       {time.map(({ name, label }, index) => (
-        <section key={index} className="text-center">
+        <section key={index} className="text-center text-shadow-sm">
           <p className="mb-4 text-2xl">{label}</p>
           <span className="font-bold text-6xl">{formatNumber(timeLeft[name])}</span>
         </section>
