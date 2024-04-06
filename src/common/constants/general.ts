ebb3de575124bff6537b39ab6ef253a9bd488fb2
Samuel Llibre Santos <kuzenshi@gmail.com>
2024-04-06 12:45:51 -0300

    feat(constants): 🏷️ include twitch in VARIANT enum and add TWITCH_STATUS_URL

--- src/common/constants/general.ts
+++ src/common/constants/general.ts
@@ -1,10 +1,13 @@
 export enum VARIANT {
   PRIMARY = 'Primary',
   SECONDARY = 'Secondary',
-  GHOST = 'Ghost'
+  GHOST = 'Ghost',
+  twitch = 'twitch'
 }
 
 export enum ORIENTATION {
   VERTICAL = 'vertical',
   HORIZONTAL = 'horizontal'
 }
+
+export const TWITCH_STATUS_URL = 'https://midudev-apis.midudev.workers.dev/uptime/afor_digital';
