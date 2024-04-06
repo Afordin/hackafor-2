1123fff34fc2d0184b4c4b9913b6aa50893cabd5
Samuel Llibre Santos <kuzenshi@gmail.com>
2024-04-06 13:41:46 -0300

    chore(common): 🚚 move components related constants to constants/components folder

---
+++ src/common/constants/components/avatar.ts
@@ -0,0 +1,5 @@
+export enum AvatarSize {
+  sm = 'sm',
+  md = 'md',
+  lg = 'lg'
+}
---
+++ src/common/constants/components/index.ts
@@ -0,0 +1,4 @@
+export * from './avatar';
+export * from './button';
+export * from './carousel';
+export * from './tag';
