cd5bc35700debc2261bdefd757b38fac930fdf2b
Samuel Llibre Santos <kuzenshi@gmail.com>
2024-04-06 14:46:18 -0300

    feat(page): ✨ implement spinner

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,12 +1,11 @@
 import { useState } from 'react';
 import { ButtonSize, cn, Project, ProjectRoles, ProjectStatus, useProjects, Variant } from '@common';
-import { Button, ProjectCard, ToggleButtonGroup } from '@components';
+import { Button, ProjectCard, Spinner, ToggleButtonGroup } from '@components';
 import { RootLayout } from '@layouts';
 import { filterBy } from './utils/filterBy';
 
 export const Projects = () => {
-  // TODO: Implement a Loading State
-  const { projects } = useProjects();
+  const { projects, isLoading } = useProjects();
   const [filter, setFilter] = useState<string[]>([]);
   const [isActive, setIsActive] = useState(true);
   const activeProjects = projects?.filter((project) => project.status === ProjectStatus.pending);
@@ -39,6 +38,14 @@ export const Projects = () => {
         />
       );
     });
+  const renderLoading = () => {
+    return (
+      <div className="absolute inset-0 mx-auto mt-20 grid place-items-center gap-4">
+        <p>Cargando...</p>
+        <Spinner className="" />
+      </div>
+    );
+  };
 
   const resetFilter = () => setFilter([]);
   const toggleFilterRole = (roleToToggle: string) => {
@@ -52,7 +59,7 @@ export const Projects = () => {
 
   return (
     <RootLayout>
-      <main id="projects">
+      <main id="projects" className="min-h-[90svh]">
         <div className="container mx-auto relative z-2 w-full py-40 max-w-7xl mx-auto gap-y-16 font-dmsans text-white px-5">
           {/* TODO: Change to FIGMA element */}
 
@@ -120,7 +127,8 @@ export const Projects = () => {
           </section>
 
           {/* TODO: add the grid-cols arbitrary to UNOCSS config */}
-          <section className="grid sm:grid-cols-[repeat(auto-fit,_minmax(390px,1fr))] gap-6 pt-12 justify-items-center">
+          <section className="grid sm:grid-cols-[repeat(auto-fit,_minmax(390px,1fr))] gap-6 pt-12 justify-items-center relative h-full w-full">
+            {isLoading && renderLoading()}
             {isActive ? renderProjects(activeProjects) : renderProjects(closedProjects)}
           </section>
         </div>
