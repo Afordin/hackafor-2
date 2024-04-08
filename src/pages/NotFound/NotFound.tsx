9c3924b995cc5f0704b5483d8722bc54cc796124
LUISEDOCCOR <luiseduardoocegueda@gmail.com>
2024-04-08 11:52:20 -0600

    feat: add hamster in not found

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -7,21 +7,47 @@ export const NotFound = () => {
   return (
     <RootLayout>
       <Background />
-      <main className="relative z-2 w-full max-w-7xl mx-auto px-5 flex flex-col justify-center items-center h-[87svh] text-cWhite font-dmsans gap-16">
-        <article className="space-y-lg text-shadow-md">
-          <h1 className="text-7xl font-semibold">Error 404</h1>
-          <h2 className="text-4xl font-semibold">PÁGINA NO ENCONTRADA</h2>
-          <p className="text-3xl">
-            ¡Hola! Lo sentimos, pero no pudimos encontrar lo que buscabas. Verifica que la dirección URL sea correcta
-          </p>
+      <main
+        className="relative z-2 w-full max-w-7xl mx-auto flex justify-center 
+      items-center h-[87svh] text-cWhite font-dmsans"
+      >
+        <article className="px-5">
+          <section className="space-y-lg text-shadow-md">
+            <h1 className="text-7xl font-semibold">Error 404</h1>
+            <h2 className="text-4xl font-semibold">PÁGINA NO ENCONTRADA</h2>
+            <p className="text-3xl">
+              ¡Hola! Lo sentimos, pero no pudimos encontrar lo que buscabas. Verifica que la dirección URL sea correcta
+            </p>
+          </section>
+          <section className="flex justify-center mt-8">
+            <Button size={ButtonSize.xl} variant={Variant.ghost} onClick={() => {}}>
+              <Link to={ROUTE.home} className="flex gap-2 items-center">
+                <span>IR A INICIO</span>
+                <div className="i-lucide:arrow-up-right  w-8 h-8 bg-gradient-to-rb from-primary-600 to-secondary-500" />
+              </Link>
+            </Button>
+          </section>
         </article>
-        <article className="flex justify-center mt-8">
-          <Button size={ButtonSize.xl} variant={Variant.ghost} onClick={() => {}}>
-            <Link to={ROUTE.home} className="flex gap-2 items-center">
-              <span>IR A INICIO</span>
-              <div className="i-lucide:arrow-up-right  w-8 h-8 bg-gradient-to-rb from-primary-600 to-secondary-500" />
-            </Link>
-          </Button>
+        <article className="hidden md:block">
+          {/* https://codepen.io/jkantner/pen/wvqeXrQ */}
+          <div className="wheel-and-hamster" role="img" aria-label="Orange and tan hamster running in a metal wheel">
+            <div className="wheel"></div>
+            <div className="hamster">
+              <div className="hamster__body">
+                <div className="hamster__head">
+                  <div className="hamster__ear"></div>
+                  <div className="hamster__eye"></div>
+                  <div className="hamster__nose"></div>
+                </div>
+                <div className="hamster__limb hamster__limb--fr"></div>
+                <div className="hamster__limb hamster__limb--fl"></div>
+                <div className="hamster__limb hamster__limb--br"></div>
+                <div className="hamster__limb hamster__limb--bl"></div>
+                <div className="hamster__tail"></div>
+              </div>
+            </div>
+            <div className="spoke"></div>
+          </div>
         </article>
       </main>
     </RootLayout>
