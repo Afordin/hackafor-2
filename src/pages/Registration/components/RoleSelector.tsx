338a9aa202e0aa1e62eaf76e40565982ce63411c
Samuel Llibre Santos <kuzenshi@gmail.com>
2024-04-13 19:10:09 -0300

    refactor(RoleSelector): ♻️ enhance type safety and readability
    Introduce the RoleSelectorProps interface to define props clearly,
    utilize the Role type for role definitions, and update button styling
    to include text capitalization. These changes improve the component's
    type safety, readability, and ensure consistency with design guidelines.

--- src/pages/Registration/components/RoleSelector.tsx
+++ src/pages/Registration/components/RoleSelector.tsx
@@ -1,7 +1,35 @@
-import { cn, FormFieldState, Variant } from '@common';
+import { cn, FormFieldState, Role, Variant } from '@common';
 import { Button } from '@components';
 
-export const RoleSelector = ({ roles, selectedRole, onRoleChange, fieldState, assistiveText }) => {
+interface RoleSelectorProps {
+  /**
+   * The roles that can be selected.
+   */
+  roles: Array<Role>;
+
+  /**
+   * The currently selected role.
+   */
+  selectedRole: string;
+
+  /**
+   * Callback function invoked when the selected role changes.
+   * @param role The newly selected role.
+   */
+  onRoleChange: (role: Role) => void;
+
+  /**
+   * The state of the form field, which includes properties like validity and error messages.
+   */
+  fieldState: FormFieldState;
+
+  /**
+   * Optional text to provide assistance or context for the role selector.
+   */
+  assistiveText?: string;
+}
+
+export const RoleSelector = ({ roles, selectedRole, onRoleChange, fieldState, assistiveText }: RoleSelectorProps) => {
   const classes = {
     assistiveText: cn('mt-2 text-xs font-medium absolute -bottom-6', {
       'text-slate-600': fieldState === FormFieldState.default,
@@ -11,13 +39,13 @@ export const RoleSelector = ({ roles, selectedRole, onRoleChange, fieldState, as
   };
   return (
     <div className="relative">
-      <div className="flex gap-4 flex-wrap justify-center">
+      <div className="flex gap-4 flex-wrap">
         {roles.map((role) => (
           <Button
             key={role}
             variant={selectedRole === role ? Variant.primary : Variant.secondary}
             hasBorder
-            className={'whitespace-nowrap'}
+            className="whitespace-nowrap capitalize"
             innerClassName="px-2 py-1"
             onClick={() => onRoleChange(role)}
           >
