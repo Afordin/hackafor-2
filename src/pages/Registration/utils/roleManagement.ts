faf621125e7916d49186d28b1a3f6f1a91b1aabc
Samuel Llibre Santos <kuzenshi@gmail.com>
2024-04-13 19:14:41 -0300

    refactor(role management): ♻️ use RequiredRoles for role state management
    Replace the custom RoleState type with the standard RequiredRoles type
    from the common module to ensure consistent type usage and enhance
    code maintainability in role management functions.

--- src/pages/Registration/utils/roleManagement.ts
+++ src/pages/Registration/utils/roleManagement.ts
@@ -1,6 +1,21 @@
-export type RoleState = { [key: string]: number };
-export type SetRoleState = React.Dispatch<React.SetStateAction<RoleState>>;
+import { RequiredRoles } from '@common';
 
+type SetRoleState = React.Dispatch<React.SetStateAction<RequiredRoles>>;
+
+/**
+ * The function `addWantedRole` increments the count of a specific role in a set state if the current
+ * number of members is less than 3.
+ * @param {string} role - The `role` parameter in the `addWantedRole` function represents the role that
+ * you want to add to the set of wanted roles. It is a string value that specifies the role you are
+ * interested in adding.
+ * @param {number} currentNumberOfMembers - The `currentNumberOfMembers` parameter represents the
+ * current number of members in a group or team. This value is used in the `addWantedRole` function to
+ * determine whether a new role can be added based on a condition (in this case, if the current number
+ * of members is less than
+ * @param {SetRoleState} setWantedRoles - `setWantedRoles` is a function that is used to update the
+ * state of wanted roles in a component. It takes a new state value as an argument and updates the
+ * state accordingly.
+ */
 export const addWantedRole = (role: string, currentNumberOfMembers: number, setWantedRoles: SetRoleState) => {
   setWantedRoles((prev) => {
     if (currentNumberOfMembers < 3) {
@@ -11,6 +26,15 @@ export const addWantedRole = (role: string, currentNumberOfMembers: number, setW
   });
 };
 
+/**
+ * The function `removeWantedRole` decreases the count of a specific role in a set of roles.
+ * @param {string} role - The `role` parameter is a string that represents the role that you want to
+ * remove from the set of wanted roles.
+ * @param {SetRoleState} setWantedRoles - The `setWantedRoles` parameter is a function that allows you
+ * to update the state of wanted roles in your application. It takes a new state value as an argument
+ * and updates the state accordingly. In the provided function `removeWantedRole`, it is used to update
+ * the count of a specific
+ */
 export const removeWantedRole = (role: string, setWantedRoles: SetRoleState) => {
   setWantedRoles((prev) => {
     const newCount = (prev[role] || 0) > 0 ? prev[role] - 1 : prev[role];
